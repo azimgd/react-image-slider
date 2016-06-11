32dffe14f85a883704ec9a17634223c7ef611087
Azim Gadzhiagayev <me@azimgd.com>
2016-06-11 23:46:22 +0200

    Refactor to es6

--- src/image-slider.js
+++ src/image-slider.js
@@ -1,43 +1,52 @@
 import React from 'react';
 
-export default React.createClass({
-  getDefaultProps() {
-    return {
-      isInfinite: true,
-      delay: 5000,
-      visibleItems: 4
-    }
-  },
-  getInitialState() {
-    return {
+class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.scrollLeft = this.scrollLeft.bind(this);
+    this.scrollRight = this.scrollRight.bind(this);
+    this.updatePosition = this.updatePosition.bind(this);
+    this.setVisibleItems = this.setVisibleItems.bind(this);
+    this.sliderStyle = this.sliderStyle.bind(this);
+    this.isOpaque = this.isOpaque.bind(this);
+    this.animate = this.animate.bind(this);
+
+    this.state = {
       images: [],
       currentPosition: 0,
       interval: null
     };
-  },
+  }
+
   componentDidMount() {
     this.animate();
     this.setVisibleItems(this.props.visibleItems);
 
     window.addEventListener('resize', this.setVisibleItems.bind(this, this.props.visibleItems));
-  },
+  }
+
   componentWillMount() {
     const images = (this.props.images || []).map((image, count) => {
       return image + `?rscver${count}`;
     });
     this.setState({images});
-  },
+  }
+
   componentWillUnmount() {
     window.removeEventListener('resize', this.setVisibleItems.bind(this, this.props.visibleItems));
-  },
+  }
+
   scrollLeft() {
     this.updatePosition(this.state.currentPosition - 1);
     this.animate();
-  },
+  }
+
   scrollRight() {
     this.updatePosition(this.state.currentPosition + 1);
     this.animate();
-  },
+  }
+
   updatePosition(position) {
     const whole = position + (this.state.visibleItems || this.props.visibleItems);
 
@@ -54,10 +63,12 @@ export default React.createClass({
     }
 
     this.setState({ currentPosition: position });
-  },
+  }
+
   calculateShift(offset, amount) {
     return offset * amount;
-  },
+  }
+
   setVisibleItems(visibleItems) {
     const windowWidth = window.innerWidth;
 
@@ -66,7 +77,8 @@ export default React.createClass({
     } else {
       this.setState({ visibleItems });
     }
-  },
+  }
+
   sliderStyle(classname) {
     const items = document.getElementsByClassName(classname);
     const itemWidth = (items[0]) ? items[0].offsetWidth : 0;
@@ -74,13 +86,15 @@ export default React.createClass({
     const transform = `translateX(-${shift}px)`;
 
     return { transform };
-  },
+  }
+
   isOpaque(key) {
     const nextPosition = (this.state.visibleItems || this.props.visibleItems) + this.state.currentPosition;
     const opaque = this.state.images.slice(this.state.currentPosition, nextPosition);
 
     return (opaque.indexOf(this.state.images[key]) !== -1);
-  },
+  }
+
   animate() {
     if (this.state.interval) {
       clearInterval(this.state.interval);
@@ -92,32 +106,39 @@ export default React.createClass({
 
     const interval = setInterval(this.scrollRight, this.props.delay);
     this.setState({ interval });
-  },
+  }
+
   render() {
     const sliderStyle = this.sliderStyle('rsc-slider-item');
-    const images = this.state.images;
+    const { images } = this.state;
 
     return (
       <div className="rsc-container">
         <div className="rsc-slider" style={sliderStyle}>
           {images.map((item, key) => {
-            const isOpaque = this.isOpaque(key);
-            const itemClass = (isOpaque) ? 'rsc-slider-item' : 'rsc-slider-item rsc-slider-item_transparent';
+            const itemClass = this.isOpaque(key) ? 'rsc-slider-item' : 'rsc-slider-item rsc-slider-item_transparent';
             const imgWidth = 100 / (this.state.visibleItems || this.props.visibleItems);
-            const itemStyle = {'flex': `0 0 ${imgWidth}%`};
 
-            return <div className={itemClass} style={itemStyle} key={key}>
+            return <div className={itemClass} key={key} style={{'flex': `0 0 ${imgWidth}%`}}>
               <img src={item} className="rsc-slider-item-img" />
             </div>
           })}
         </div>
         {images.length > (this.state.visibleItems || this.props.visibleItems) ?
-        <div>
-          <div className="rsc-navigation rsc-navigation_left rsc-arrow_left" onClick={this.scrollLeft}></div>
-          <div className="rsc-navigation rsc-navigation_right rsc-arrow_right" onClick={this.scrollRight}></div>
-        </div>
-        :null}
+          <div>
+            <div className="rsc-navigation rsc-navigation_left rsc-arrow_left" onClick={this.scrollLeft}></div>
+            <div className="rsc-navigation rsc-navigation_right rsc-arrow_right" onClick={this.scrollRight}></div>
+          </div>
+        : null}
       </div>
     );
   }
-});
+}
+
+Slider.defaultProps = {
+  isInfinite: true,
+  delay: 5000,
+  visibleItems: 4,
+};
+
+export default Slider;
